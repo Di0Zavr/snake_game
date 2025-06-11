756f48781394a1a2a327b9de409ef960f69cbd84
vendatt0r <vasyok03020454@gmail.com>
2025-06-12 01:56:44 +0300

    Приветственный экран

--- app/src/main/java/com/example/snakegame/MainActivity.kt
+++ app/src/main/java/com/example/snakegame/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.snakegame
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.snakegame.ui.theme.SnakeGameTheme
@@ -13,14 +13,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SnakeGameTheme {
-                val viewModel = viewModel<SnakeGameViewModel>()
-                val state by viewModel.state.collectAsStateWithLifecycle()
-                SnakeGameScreen(
-                    state = state,
-                    onEvent = viewModel::onEvent
-                )
+                var isGameStarted by remember { mutableStateOf(false) }
+
+                if (!isGameStarted) {
+                    WelcomeScreen(
+                        onStartClick = { isGameStarted = true }
+                    )
+                } else {
+                    val viewModel = viewModel<SnakeGameViewModel>()
+                    val state by viewModel.state.collectAsStateWithLifecycle()
+                    SnakeGameScreen(
+                        state = state,
+                        onEvent = viewModel::onEvent
+                    )
+                }
             }
         }
     }
 }
+
 
